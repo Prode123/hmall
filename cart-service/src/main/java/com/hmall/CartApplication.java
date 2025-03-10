3a5701c21e70ce2df72f7d91e53d6bb8f3cade2e
Prode123 <1002411753@qq.com>
2025-03-10 23:17:19 +0800

    OpenFeign全局日志配置

--- cart-service/src/main/java/com/hmall/CartApplication.java
+++ cart-service/src/main/java/com/hmall/CartApplication.java
@@ -1,4 +1,5 @@
 package com.hmall;
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @Data 2025/03/09 18:09
  */
 
-@EnableFeignClients(basePackages = "com.hmall.api.client")
+@EnableFeignClients(basePackages = "com.hmall.api.client" , defaultConfiguration = DefaultFeignConfig.class)
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
