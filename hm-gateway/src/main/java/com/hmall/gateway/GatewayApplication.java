364ed4c2893bf37804c6e3ecb817a0cb0f5e0b22
Prode123 <1002411753@qq.com>
2025-03-11 22:29:00 +0800

    搭建网关

---
+++ hm-gateway/src/main/java/com/hmall/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author LiTong(Prode)
+ * @Data 2025/03/11 22:20
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
