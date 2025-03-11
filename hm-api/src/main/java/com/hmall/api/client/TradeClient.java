47fe4236690398a5a986175bd6f64fa9b9e65c06
Prode123 <1002411753@qq.com>
2025-03-11 19:06:34 +0800

    微服务拆分之pay-service

---
+++ hm-api/src/main/java/com/hmall/api/client/TradeClient.java
@@ -0,0 +1,17 @@
+package com.hmall.api.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+/**
+ * @Description
+ * @Author lt
+ * @Data 2025/03/11 19:01
+ */
+
+@FeignClient("trade-service")
+public interface TradeClient {
+    @PutMapping("/orders/{orderId}")
+    void markOrderPaySuccess(@PathVariable("orderId") Long orderId);
+}
